0352903d2815a026d2b394cf587a0187b1854f38
Vikram Nadig <Vikram.Nadig@comparisontech.com>
2021-06-09 22:54:01 +0100

    adding the initial page with some query metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,11 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'My New Blog',
+    description: 'This is my awesome blog I made from scratch!'  
+  },
   plugins: [],
 }
